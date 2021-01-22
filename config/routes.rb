e812715e43c67746b675a19e1843150679aafedf
yani82 <yani.iman1@gmail.com>
2021-01-22 01:22:30 -0500

    added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
 
